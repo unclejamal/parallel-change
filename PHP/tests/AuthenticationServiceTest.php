22d2fde1ff4931740313a2db7ba6292b7285cb29
Sergey Kudashev <kudashevs@gmail.com>
2026-02-11 01:16:13 +0300

    fix styles in PHP version

--- PHP/tests/AuthenticationServiceTest.php
+++ PHP/tests/AuthenticationServiceTest.php
@@ -1,20 +1,22 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 use ParallelChange\AuthenticationService;
 
-class AuthenticationServiceTest extends PHPUnit_Framework_TestCase {
+class AuthenticationServiceTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdministratorIsAlwaysAuthenticated()
+    {
+        $authenticationService = new AuthenticationService();
+        $adminId = 12345;
+        $this->assertTrue($authenticationService->isAuthenticated($adminId));
+    }
 
-	public function testAdministratorIsAlwaysAuthenticated() {
-		$authenticationService = new AuthenticationService();
-		$adminId = 12345;
-		$this->assertTrue($authenticationService->isAuthenticated($adminId));
-	}
-
-	public function testNormalUserIsNotInitiallyAuthenticated() {
-		$authenticationService = new AuthenticationService();
-		$userId = 11111;
-		$this->assertFalse($authenticationService->isAuthenticated($userId));
-	}
-
+    public function testNormalUserIsNotInitiallyAuthenticated()
+    {
+        $authenticationService = new AuthenticationService();
+        $userId = 11111;
+        $this->assertFalse($authenticationService->isAuthenticated($userId));
+    }
 }
